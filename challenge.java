53bf6ea28cd4a854437681182684de7b7e6d1fc5
Wima Hayfa Jasmeen <152079153+Itswimhjee@users.noreply.github.com>
2024-08-14 13:55:32 +0700

    Add files via upload
    MEET04

---
+++ challenge.java
@@ -0,0 +1,19 @@
+package MEET04;
+
+public class challenge {
+    public static void main(String[] args) throws Exception {
+        // byte a = 100;
+        // boolean nilai1 = a > 10 && a < 15;
+        // boolean nilai2 = a > 13 && a < 20;
+        // boolean nilai3 = a > 20 && a < 30;
+        // boolean hasil = nilai1 && nilai2 || nilai3;
+        // System.out.println(hasil);
+
+        byte a = 100;
+        boolean nilai1 = a > 1 && a < 3;
+        boolean nilai2 = a > 88 && a < 101;
+        boolean nilai3 = a > 99 && a < 101;
+        boolean hasil = nilai1 && nilai2 || nilai3;
+        System.out.println(hasil);
+    }
+}
